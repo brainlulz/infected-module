4d888dae335344f0cb648c0b14bb7430ac020ba3
Alex Labonne <>
2019-02-18 19:51:11 +0100

    chore: init infected module

---
+++ README.md
@@ -0,0 +1,3 @@
+### This is a fake infected module
+This module is just used for a POC.
+Do not use it.
---
+++ index.js
@@ -0,0 +1,3 @@
+const runMaliciousModule = require('malicious-module');
+runMaliciousModule('tutu');
+console.log("look at your /tmp directory, a fake malicious file is implemented");
